154c0101da7a67797b329b78238c8367eec09360
Mastmex <mastmex42@gmail.com>
2024-05-25 21:17:11 +0300

    init

---
+++ source/classes/table/table.hpp
@@ -0,0 +1,14 @@
+#include <map>
+#include <string>
+
+class table
+{
+private:
+    std::map<std::string, std::string> table_map;
+    std::string table_name;
+public:
+    table(/* args */);
+    
+};
+
+
---
+++ source/classes/table_parser/table_parser.hpp
@@ -0,0 +1,11 @@
+#include "table.hpp"
+class table_parser
+{
+private:
+    /* data */
+public:
+    table_parser(/* args */);
+    ~table_parser();
+    table get_next_table();
+};
+
